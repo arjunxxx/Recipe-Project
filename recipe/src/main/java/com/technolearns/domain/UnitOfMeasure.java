26a9cb413299fb203a1e20445f10d8a7cd9f2cfa
arjunxxx <arjunrajeev1@gmail.com>
2021-06-03 10:32:02 +0530

    uom domain commit

--- recipe/src/main/java/com/technolearns/domain/UnitOfMeasure.java
+++ recipe/src/main/java/com/technolearns/domain/UnitOfMeasure.java
@@ -1,5 +1,30 @@
 package com.technolearns.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UnitOfMeasure {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String unit;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
+	
 }
