0cd7c0debc94f990dd34364297a28a1c9eca563e
arjunxxx <arjunrajeev1@gmail.com>
2021-06-03 12:25:18 +0530

    Repository interfaces added

---
+++ recipe/src/main/java/com/technolearns/repositories/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.technolearns.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.technolearns.domain.Recipe;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+}
---
+++ recipe/src/main/java/com/technolearns/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,9 @@
+package com.technolearns.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.technolearns.domain.UnitOfMeasure;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+}
